eb3421e9ec1321007c5be2fcf1bdf01e2c055ab6
goldhand <mandmod@gmail.com>
2022-09-09 21:14:10 +0700

    test22

---
+++ ifelse810.py
@@ -0,0 +1,8 @@
+num1 = float(input('กรุณากรอกจำนวนเงินให้หน่อย num1: = '))
+num2 = float(input('กรุณากรอกจำนวนเงินให้หน่อย num2: = '))
+if num1 > num2:
+    print('Num1 > num2')
+elif num1 < num2:
+    print('Num1 < num2')
+else:
+        print('Num1 = num2')
---
+++ ifelse811.py
@@ -0,0 +1,6 @@
+str1 = str(input('กรุณากรอกสายอักขระที่ 1 str1: = '))
+str2 = str(input('กรุณากรอกสายอักขระที่ 2 str2: = '))
+if len(str1)==len(str2):
+    print('Same')
+else:
+    print('not same')
