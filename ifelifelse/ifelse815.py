855b0dc397603c696a8ef7a09d813c6df8734345
mandmod <mandmod@gmail.com>
2022-09-12 07:22:38 +0700

    Move File if to Folder

---
+++ ifelifelse/ifelse815.py
@@ -0,0 +1,5 @@
+num1 = float(input('กรุณากรอกจำนวนเงินให้หน่อย num1: = '))
+num2 = float(input('กรุณากรอกจำนวนเงินให้หน่อย num1: = '))
+num3 = float(input('กรุณากรอกจำนวนเงินให้หน่อย num3: = '))
+if num1 + num2 > num3:
+    print('num1+num2> num3')
