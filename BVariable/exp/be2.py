9e0d195ad0e42ce1aa6b5a7ba5b61500aad92763
goldhand <mandmod@gmail.com>
2022-12-18 21:40:31 +0700

    Variable

---
+++ BVariable/exp/be2.py
@@ -0,0 +1,16 @@
+c = d = e = 5
+print('c =', c)
+print('d =', d)
+print('e =', e)
+
+f = 6;
+g = 7;
+h = 8;
+print('f =', f)
+print('g =', g)
+print('h =', h)
+
+i, j, k = 9, 10, 11
+print('i =', i)
+print('j =', j)
+print('k =', k)
