e864dab153e7b7b7c1e40a22b8306ee89188e78d
mandmod <mandmod@gmail.com>
2022-12-19 10:27:48 +0700

    DataStructure

---
+++ CDataStructure/exp/ce121CreateString.py
@@ -0,0 +1,4 @@
+char1 = 'Hello World'
+char2 = '0123456789'
+print(type(char1))
+print(type(char2))
---
+++ CDataStructure/exp/ce122ReadString.py
@@ -0,0 +1,5 @@
+char3 = '0123456789'
+print(char3[0])
+print(char3[5])
+print(char3[-1])
+print(char3[-2])
